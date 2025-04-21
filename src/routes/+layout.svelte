b6bd61aeff9a6fefbad7eacc814e5e49cd0a06b5
Matt Gleich <git@mattglei.ch>
2025-04-21 00:35:41 -0400

    feat: basic UI design
    Signed-off-by: Matt Gleich <git@mattglei.ch>

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import '@gleich/ui/styles.css';
+	import '../styles/global.css';
+	import '../styles/fonts.css';
+
+	const { children }: { children: Snippet } = $props();
+</script>
+
+<main>
+	{@render children()}
+</main>
+
+<style>
+	main {
+		min-height: 100vh;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,39 @@
-<h1>pomo</h1>
+<script>
+	let running = $state(false);
+</script>
+
+<div class="container">
+	<h1 class={running ? 'running-title' : 'starting-title'}>
+		<span contenteditable>25</span>:<span contenteditable>00</span>
+	</h1>
+
+	<button class="state-button"> Start </button>
+</div>
+
+<style>
+	h1 {
+		font-weight: 600;
+	}
+
+	.container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		height: 100vh;
+		gap: 15px;
+	}
+
+	.running-title {
+		color: var(--green-foreground);
+	}
+
+	.starting-title {
+		color: grey;
+	}
+
+	.state-button {
+		width: 200px;
+		padding: 10px;
+	}
+</style>
